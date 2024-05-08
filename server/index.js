14ffbc26b6affbf5f79cda823145c93d0704081b
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-05-08 21:14:25 +0900

    creating our server on 8080

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log("App running on 8080");
+});
