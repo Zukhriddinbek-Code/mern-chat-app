83b2445b16e1f19fee59dde350a0f9790645fa3e
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-05-12 18:09:32 +0900

    creating helper fn to decode token

---
+++ server/helper/userDetailsToken.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+import UserModel from "../models/User.js";
+
+const userDetailsToken = async (token) => {
+  if (!token) {
+    return {
+      message: "session out",
+      logout: true,
+    };
+  }
+
+  const decodeUser = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  const userDoc = await UserModel.findById(decodeUser.id);
+  return userDoc;
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,12 +2,15 @@ import express from "express";
 
 const router = express.Router();
 
-import { signup, emailCheck } from "../controllers/auth.js";
+import { signup, emailCheck, passwordCheck } from "../controllers/auth.js";
 
 //create user api
 router.post("/signup", signup);
 
 //checking user email
 router.post("/email", emailCheck);
+
+//checking password and setting jwt
+router.post("/password", passwordCheck);
 
 export default router;
