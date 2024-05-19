f1c7bec9c8fdbcdc3b77e9e54e881cb4b9c7f07a
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-05-19 17:29:07 +0900

    creating form for name field

--- frontend/src/layout/index.js
+++ frontend/src/layout/index.js
@@ -4,7 +4,7 @@ import logo from "../assets/logo.png";
 const AuthLayout = ({ children }) => {
   return (
     <>
-      <header className="flex justify-center item-center py-3 h-20 shadow-md">
+      <header className="flex justify-center item-center py-3 h-20 shadow-md bg-white">
         <img src={logo} alt="logo" width="180" height="60" />
       </header>
       {children}
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,5 +1,43 @@
+import { useState } from "react";
+
 const Register = () => {
-  return <div>Register</div>;
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    profile_pic: "",
+  });
+
+  const handleOnChange = (e) => {
+    const { name, email } = e.target.value;
+    setData((prev) => {
+      return { ...prev, [name]: name };
+    });
+  };
+
+  return (
+    <div className="mt-5">
+      <div className="bg-white w-full max-w-sm mx-2 rounded overflow-hidden p-4">
+        <h3>Welcome to Chat App!</h3>
+
+        <form>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="name">Name :</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="john doe"
+              className="bg-slate-100 px-2 py-1 focus:outline-primary"
+              value={data.name}
+              onChange={handleOnChange}
+              required
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
