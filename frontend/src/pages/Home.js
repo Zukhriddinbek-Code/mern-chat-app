f501e9fce3e3ef7ed99c602f52dafd52bbe6feee
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-05-19 16:07:05 +0900

    creating logo layout comp for register, email, password

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      {/* message component */}
+      <section>
+        <Outlet />
+      </section>
+    </div>
+  );
 };
 
 export default Home;
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -5,15 +5,37 @@ import CheckEmail from "../pages/CheckEmail";
 import CheckPassword from "../pages/CheckPassword";
 import Home from "../pages/Home";
 import Message from "../components/Message";
+import AuthLayout from "../layout";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { path: "register", element: <Register /> },
-      { path: "email", element: <CheckEmail /> },
-      { path: "password", element: <CheckPassword /> },
+      {
+        path: "register",
+        element: (
+          <AuthLayout>
+            <Register />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "email",
+        element: (
+          <AuthLayout>
+            <CheckEmail />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "password",
+        element: (
+          <AuthLayout>
+            <CheckPassword />
+          </AuthLayout>
+        ),
+      },
       {
         path: "",
         element: <Home />,
