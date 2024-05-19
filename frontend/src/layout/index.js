38b65cdf409629f4c7b8313638fcd9195c01b763
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-05-19 17:01:24 +0900

    renderig logo

--- frontend/src/layout/index.js
+++ frontend/src/layout/index.js
@@ -1,9 +1,12 @@
 import React from "react";
+import logo from "../assets/logo.png";
 
 const AuthLayout = ({ children }) => {
   return (
     <>
-      <div>Logo</div>
+      <header className="flex justify-center item-center py-3 h-20 shadow-md">
+        <img src={logo} alt="logo" width="180" height="60" />
+      </header>
       {children}
     </>
   );
