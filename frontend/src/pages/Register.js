3e9febb2617b0c8eb475b5122db5a9c4120dda79
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-05-20 22:43:55 +0900

    input field email, password

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 
 const Register = () => {
@@ -9,7 +10,7 @@ const Register = () => {
   });
 
   const handleOnChange = (e) => {
-    const { name, email } = e.target.value;
+    const { name, email, password } = e.target.value;
     setData((prev) => {
       return { ...prev, [name]: name };
     });
@@ -20,9 +21,13 @@ const Register = () => {
       <div className="bg-white w-full max-w-sm mx-2 rounded overflow-hidden p-4">
         <h3>Welcome to Chat App!</h3>
 
-        <form>
+        <form
+          className="grid gap-4 mt-4
+         "
+        >
+          {/* name input field */}
           <div className="flex flex-col gap-1">
-            <label htmlFor="name">Name :</label>
+            <label htmlFor="name">Name</label>
             <input
               type="text"
               id="name"
@@ -32,6 +37,39 @@ const Register = () => {
               value={data.name}
               onChange={handleOnChange}
               required
+              autocomplete="off"
+            />
+          </div>
+
+          {/* email input field */}
+          <div className="flex flex-col gap-1">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="johndoe@chat.com"
+              className="bg-slate-100 px-2 py-1 focus:outline-primary"
+              value={data.email}
+              onChange={handleOnChange}
+              required
+              autocomplete="off"
+            />
+          </div>
+
+          {/* password input field */}
+          <div className="flex flex-col gap-1">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="*********"
+              className="bg-slate-100 px-2 py-1 focus:outline-primary"
+              value={data.password}
+              onChange={handleOnChange}
+              required
+              autocomplete="off"
             />
           </div>
         </form>
