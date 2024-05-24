6705f8c5a2166faee10e0daead9b47edddd6cfe4
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-05-24 23:13:11 +0900

    creating helper fn to upload photos and videos

--- frontend/src/helpers/uploadFile.js
+++ frontend/src/helpers/uploadFile.js
@@ -1,1 +1,16 @@
-const url = `https://api.cloudinary.com/v1_1/${process.env.cloud_name}/:action/upload`;
+const url = `https://api.cloudinary.com/v1_1/${process.env.cloud_name}/auto/upload`;
+
+const uploadFile = async (file) => {
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", "chat-app-file");
+
+  const response = await fetch(url, {
+    body: formData,
+  });
+  const responseData = await response.json();
+
+  return responseData;
+};
+
+export default uploadFile;
