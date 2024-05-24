bc2b292de74b6050ba50eae8337e899c650dee32
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-05-24 23:00:38 +0900

    deleted the api keys from anyone else's access

--- frontend/src/helpers/uploadFile.js
+++ frontend/src/helpers/uploadFile.js
@@ -1,9 +1,1 @@
 const url = `https://api.cloudinary.com/v1_1/${process.env.cloud_name}/:action/upload`;
-
-// import { v2 as cloudinary } from "cloudinary";
-
-// cloudinary.config({
-//   cloud_name: "demnrcdiy",
-//   api_key: "544655322591413",
-//   api_secret: "ggCOan8LzF08fPxNWlgZgxmZk1Y",
-// });
