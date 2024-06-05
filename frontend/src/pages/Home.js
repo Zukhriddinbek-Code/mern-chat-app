0e7631936465e8078abf3936affdc2bf62fc325c
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-06-05 20:49:11 +0900

    getting user data with tokens

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,23 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import axios from "axios";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 const Home = () => {
+  const fetchUserDetails = async () => {
+    try {
+      const url = `${process.env.REACT_APP_backend_url}auth/user-details`;
+      const userDetails = await axios({ url: url, withCredentials: true });
+      console.log(userDetails);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserDetails();
+  });
+
   return (
     <div>
       Home
