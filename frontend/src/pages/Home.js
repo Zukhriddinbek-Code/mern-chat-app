a1e1b4947a78974b951bca8ea5f0f1cb5b77cb14
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-06-10 22:06:24 +0900

    checking if not data got, then logging user out

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,18 +1,29 @@
 /* eslint-disable no-unused-vars */
 import axios from "axios";
 import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Outlet, useNavigate } from "react-router-dom";
+import { logout, setUser } from "../redux/userSlice";
 
 const Home = () => {
   const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   console.log(user);
 
   const fetchUserDetails = async () => {
     try {
       const url = `${process.env.REACT_APP_backend_url}auth/user-details`;
       const userDetails = await axios({ url: url, withCredentials: true });
-      console.log(userDetails);
+
+      dispatch(setUser(userDetails.data.data));
+
+      if (userDetails.data.logout) {
+        dispatch(logout());
+        navigate("/email");
+        // localStorage.removeItem("token");
+      }
+      // console.log(userDetails);
     } catch (error) {
       console.log(error);
     }
