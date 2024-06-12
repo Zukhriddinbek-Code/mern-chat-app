7d5941f2513eb56271f56de979055250150bb003
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-06-12 15:18:23 +0900

    hovering on mouse, with title='' in react

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -4,8 +4,11 @@ const Sidebar = () => {
   return (
     <div className="w-full h-full">
       <div className="bg-slate-200 w-12 h-full rounded-tr-lg rounded-br-lg py-5">
-        <div>
-          <IoChatbubbleEllipsesSharp />
+        <div
+          className="w-12 h-12 bg-red-500 flex justify-center items-center"
+          title="chat"
+        >
+          <IoChatbubbleEllipsesSharp size={25} />
         </div>
       </div>
     </div>
