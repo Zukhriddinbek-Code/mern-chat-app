73f90d5532c647ef3030a9016bdde3245e54c415
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-07-24 19:33:22 +0900

    creating a room for online users and sending them to client side

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { Server } from "socket.io";
 import http from "http";
+import userDetailsToken from "../helper/userDetailsToken.js";
 
 export const app = express();
 
@@ -15,14 +16,24 @@ const io = new Server(server, {
 
 //socket running at http://localhost:8080/
 
+//online user?
+const onlineUser = new Set();
+
 io.on("connection", async (socket) => {
   console.log("user connected", socket.id);
 
   const token = socket.handshake.auth.token;
   //current user details
-  console.log("token", token);
+  const user = await userDetailsToken(token);
 
-  //disconnect
+  //create a room for a user
+  socket.join(user?._id);
+  onlineUser.add(user._id);
+
+  //sending data to client side
+  io.emit("onlineUser", Array.from(onlineUser));
+
+  //disconnecß
   io.on("disconnect", () => {
     console.log("user disconnected", socket.id);
   });
